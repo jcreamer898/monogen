5ef8ef2f649d3186a704f654acb05f2ec3909e01
Jonathan Creamer <jcreamer@microsoft.com>
2022-05-02 08:45:19 -0500

    format stuff

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
-import fs from 'fs';
-import path from 'path';
-import { humanId } from 'human-id';
+import fs from "fs";
+import path from "path";
+import { humanId } from "human-id";
 
 /**
  * Call cb <count> number of times times
- * @param {number} count 
- * @param {*} cb 
- * @returns 
+ * @param {number} count
+ * @param {*} cb
+ * @returns
  */
-const times = async function(count, cb) {
+const times = async function (count, cb) {
   var i = -1;
 
   while (++i < count) {
     await cb(i);
   }
 
-  return; 
+  return;
 };
 
 /**
@@ -30,38 +30,56 @@ const times = async function(count, cb) {
  */
 
 /**
- * @param {Config} config 
+ * @param {Config} config
  */
 export const createMonorepo = (plop) => async (config) => {
   const cwd = config.cwd || process.cwd();
   const outputDir = path.join(cwd, config.outputDirectory);
-  await fs.promises.mkdir(path.join(outputDir, 'packages'), {
+  await fs.promises.mkdir(path.join(outputDir, "packages"), {
     recursive: true,
   });
 
-  const templateContents = await fs.promises.readFile(path.resolve(config.packageJsonTemplate));
-  const rootPackageJsonTemplate = await fs.promises.readFile(path.resolve(config.rootPackageJsonTemplate));
-  const srcIndexContents = await fs.promises.readFile(path.resolve(config.srcTemplate));
-  const rootPackageJson = plop.renderString(rootPackageJsonTemplate.toString(), { 
-    name: config.name,
-  });
-  
-  await fs.promises.writeFile(path.join(outputDir, "package.json"), rootPackageJson);
+  const templateContents = await fs.promises.readFile(
+    path.resolve(config.packageJsonTemplate)
+  );
+  const rootPackageJsonTemplate = await fs.promises.readFile(
+    path.resolve(config.rootPackageJsonTemplate)
+  );
+  const srcIndexContents = await fs.promises.readFile(
+    path.resolve(config.srcTemplate)
+  );
+  const rootPackageJson = plop.renderString(
+    rootPackageJsonTemplate.toString(),
+    {
+      name: config.name,
+    }
+  );
+
+  await fs.promises.writeFile(
+    path.join(outputDir, "package.json"),
+    rootPackageJson
+  );
 
   times(config.howMany, async () => {
     const name = humanId({
-      separator: '-',
+      separator: "-",
     }).toLowerCase();
-    const packagePath = path.join(outputDir, 'packages', name);
+    const packagePath = path.join(outputDir, "packages", name);
 
-    const packageJson = plop.renderString(templateContents.toString(), { 
+    const packageJson = plop.renderString(templateContents.toString(), {
       humanId: name,
     });
 
-    await fs.promises.mkdir(path.resolve(packagePath, 'src'), {
+    await fs.promises.mkdir(path.resolve(packagePath, "src"), {
       recursive: true,
     });
-    await fs.promises.writeFile(path.join(packagePath, "package.json"), packageJson);
-    await fs.promises.writeFile(path.join(packagePath, "src", "index.ts"), srcIndexContents);
+    await fs.promises.writeFile(
+      path.join(packagePath, "package.json"),
+      packageJson
+    );
+    await fs.promises.writeFile(
+      path.join(packagePath, "src", "index.ts"),
+      srcIndexContents
+    );
   });
-}
+};
